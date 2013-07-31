f83cbb8f585464b9f8e54730d5f50f940af936cf
Kevin Tong <logicmd@gmail.com>
2013-08-01 00:29:56 +0800

    wip

---
+++ fetch.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib2
+
+
+def parse(manifest_content):
+    for atype in manifest_content.findall('SmoothStreamingMedia'):
+        print(atype.get('Duration'))
+
+
+def fetch_manifest(manifest_url):
+    req = urllib2.Request(manifest_url)
+    response = urllib2.urlopen(req)
+
+
+    manifest = response.read()
+    return manifest
+
+
+def fetch_sample_manifest():
+    fetch_manifest('http://ss.logicmd.net/tears/tears_of_steel_720p.ism/Manifest')
+
+if __name__ == '__main__':
+    parse(fetch_sample_manifest())
