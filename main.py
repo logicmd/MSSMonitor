e302ef85c77f7623532efd817772f5a78c15eeee
Kevin Tong <logicmd@gmail.com>
2013-09-10 11:25:55 +0800

    done sth

--- main.py
+++ main.py
@@ -86,17 +86,15 @@ def main(argv):
             url = arg
 
 
-    print 'ua ' + ua
-    print 'cookies' + cookies
-    print 'url' + url
+    #print 'ua ' + ua
+    #print 'cookies ' + cookies
+    #print 'url ' + url
 
 
-#D:\Develop\Python\MSSMonitor>
-#main.py -a Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1581.2 Safari/537.36 -c __utma=149734569.173931233.1374135637.1374156175.1374219909.3 -u http://ss.logicmd.net/live/LiveSmoothStream.isml/Manifest
+    #main.py -a "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1581.2 Safari/537.36" -c "__utma=149734569.173931233.1374135637.1374156175.1374219909.3" -u "http://ss.logicmd.net/live/LiveSmoothStream.isml/Manifest"
 
-    return
 
-    opener = opener_builder(ua, cookies)
+    opener = parser.opener_builder(ua, cookies)
 
     SSM = SmoothStreamingMedia()
     SSM.opener=opener
